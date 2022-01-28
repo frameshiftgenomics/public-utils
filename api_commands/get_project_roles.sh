c7eaebbeddecf2df0d5c61b75011d36dd1b2cf5c
Alistair Ward <u0991467@redwood5.int.chpc.utah.edu>
2022-01-27 18:18:53 -0700

    Added new api commands

--- api_commands/get_project_roles.sh
+++ api_commands/get_project_roles.sh
@@ -3,6 +3,8 @@
 TOKEN=$1
 URL=$2
 PROJECT_ID=$3
+LIMIT=$4
+PAGE=$5
 
 curl -X GET -H "Authorization: Bearer $TOKEN" \
-$URL"/v1/projects/$PROJECT_ID/roles"
+$URL"/v1/projects/$PROJECT_ID/roles?limit=$LIMIT&page=$PAGE"
