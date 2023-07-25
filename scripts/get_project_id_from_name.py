08af6be8efc48b9a8569aa902a0945e12c1545a2
Alistair Ward <u0991467@redwood5.int.chpc.utah.edu>
2023-07-25 06:33:18 -0600

    1. Minor script update

--- scripts/get_project_id_from_name.py
+++ scripts/get_project_id_from_name.py
@@ -24,10 +24,17 @@ def main():
   # Parse the mosaic configuration file
   mosaicRequired = {'MOSAIC_TOKEN': {'value': args.token, 'desc': 'An access token', 'long': '--token', 'short': '-t'},
                     'MOSAIC_URL': {'value': args.url, 'desc': 'The api url', 'long': '--url', 'short': '-u'}}
-  mosaicConfig = mosaic_config.parseConfig(args.config, mosaicRequired)
+  mosaicConfig   = mosaic_config.mosaicConfigFile(args.config)
+  mosaicConfig   = mosaic_config.commandLineArguments(mosaicConfig, mosaicRequired)
 
   # Get the project roles
-  getId(args)
+  projects = api_p.getProjectsDictNameId(mosaicConfig)
+
+  # Loop over the projects and output the ids
+  names = args.project_names.split(',')
+  for name in names:
+    if name not in projects: print(name, ': not found in Mosaic', sep = '')
+    else: print(name, ': ', projects[name], sep = '')
 
 # Input options
 def parseCommandLine():
@@ -39,46 +46,9 @@ def parseCommandLine():
   parser.add_argument('--url', '-u', required = False, metavar = "string", help = "The base url for Mosaic curl commands, up to an including \"api\". Do NOT include a trailing /")
 
   # The project name to fild the id for
-  parser.add_argument('--project_name', '-n', required = True, metavar = 'integer', help = 'The Mosaic project name to get the id for')
+  parser.add_argument('--project_names', '-n', required = True, metavar = 'integer', help = 'A comman separated list of Mosaic project names to get the ids for')
 
   return parser.parse_args()
-
-# Get the project Id
-def getId(args):
-  global mosaicConfig
-  limit = 100
-  page  = 1
-  noMatches = 0
-  projectId = False
-
-  # Get the project reference
-  try: data = json.loads(os.popen(api_p.getProjects(mosaicConfig, limit, page)).read())
-  except: fail('Could not get projects information')
-
-  # Loop over the projects
-  for project in data['data']:
-    if project['name'].startswith(args.project_name):
-      noMatches += 1
-      projectId  = project['id']
-
-  # Loop over all pages of projects
-  noPages = int(math.ceil(float(data['count']) / float(limit)))
-
-  # Loop over all necessary pages
-  for i in range(2, noPages + 1):
-    try: data = json.loads(os.popen(api_p.getProjects(mosaicConfig, limit, i)).read())
-    except: fail('Could not get projects information')
-
-    # Loop over the projects
-    for project in data['data']:
-      if project['name'].startswith(args.project_name):
-        noMatches += 1
-        projectId  = project['id']
-
-  # If there are multiple results or no results, fail
-  if noMatches == 0: fail('No projects found starting with ' + str(args.project_name))
-  elif noMatches > 1: fail('Multiple projects found starting with ' + str(args.project_name))
-  else: print(projectId)
 
 # If the script fails, provide an error message and exit
 def fail(message):
